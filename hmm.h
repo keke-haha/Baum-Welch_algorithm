f1e2421b75c32c4465f39445a6c4e3c6cd04ddfe
zhakayefu <ljxforgaokao@163.com>
2016-03-23 22:43:04 +0800

    finished hmm class

--- hmm.h
+++ hmm.h
@@ -2,6 +2,10 @@
 #include <string>
 #include <iostream>
 
+#ifdef HMM_DEBUG
+#define _HMM_DEBUG
+#endif
+
 using namespace std;
 
 typedef vector<int> Sequence;
@@ -20,6 +24,8 @@ class HMM
 		Sequence C;			// count for each corresponding sequence
 
 	public:
+	  // the number of points needed
+	  const int T = 32;
 		// constructor & destructor
 		HMM(int _K, int _N, double _thres = 0.5);
 		HMM(HMM &_hmm);
@@ -31,12 +37,11 @@ class HMM
 		bool setPi(double *_Pi);
 		bool pushSample(Sequence sample, int _C = 1);
 		Sequence getSample(int index);
-		bool generateHMM();
-		double getLikely();
+		bool generateHMM(double thres);
 
 		// match new samples
 		int match(Sequence sample);
 
 		// operators
 		void operator=(HMM &_hmm);
-}
+};
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#define _HMM_DEBUG
+
+#include <iostream>
+#include "hmm.h"
+
+int main()
+{
+	HMM hmm(2, 3);
+}
