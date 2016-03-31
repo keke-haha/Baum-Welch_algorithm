e4d6c4dda5cdcb5415da7df03631fbbfea9ec510
zkzkz@hpssd <ljxforgaokao@163.com>
2016-03-31 16:35:38 +0800

    file input

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #define _HMM_DEBUG
 
 #include <iostream>
+#include <fstream>
 #include "hmm.h"
 
-int main()
+int main(int argc, char **argv)
 {
+	ifstream input;
 	int n, k, t, m;
-	cout << "Input n,k,t: ";
-	cin >> n >> k >> t;
-	HMM hmm(k, n, t);
+	bool file = false;
 	double *a, *b, *p;
 	a = new double[n*n];
 	b = new double[n*k];
@@ -16,43 +16,108 @@ int main()
 	ZeroMemory(a, sizeof(double)*n*n);
 	ZeroMemory(b, sizeof(double)*n*k);
 	ZeroMemory(p, sizeof(double)*n);
-	cout << "Input A:";
-	for (int i = 0; i < n; i++)
+	Sequence sample;
+
+	if (argc > 1)
 	{
-		for (int j = 0; j < n; j++)
+		input.open(argv[1], ios::in);
+		if (input.fail())
 		{
-			cin >> a[i*n + j];
+			cout << "File " << argv[1] << " doesn't exist!" << endl;
+			return -1;
+		}
+		file = true;
+	}
+
+	if (file)
+	{
+		cout << "Input n,k,t: ";
+		input >> n >> k >> t;
+		cout << "Input A:";
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				input >> a[i*n + j];
+			}
+		}
+		cout << "Input B:";
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < k; j++)
+			{
+				input >> b[i*k + j];
+			}
+		}
+		cout << "Input Pi:";
+		for (int i = 0; i < n; i++)
+		{
+			input >> p[i];
 		}
 	}
-	cout << "Input B:";
-	for (int i = 0; i < n; i++)
+	else
 	{
-		for (int j = 0; j < k; j++)
+		cout << "Input n,k,t: ";
+		cin >> n >> k >> t;
+		cout << "Input A:";
+		for (int i = 0; i < n; i++)
 		{
-			cin >> b[i*k + j];
+			for (int j = 0; j < n; j++)
+			{
+				cin >> a[i*n + j];
+			}
+		}
+		cout << "Input B:";
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < k; j++)
+			{
+				cin >> b[i*k + j];
+			}
+		}
+		cout << "Input Pi:";
+		for (int i = 0; i < n; i++)
+		{
+			cin >> p[i];
 		}
 	}
-	cout << "Input Pi:";
-	for (int i = 0; i < n; i++)
-	{
-		cin >> p[i];
-	}
+
+	HMM hmm(k, n, t);
 	hmm.setA(a); hmm.setB(b); hmm.setPi(p);
 	cout << "Input the number of samples: ";
-	cin >> m;
-	Sequence sample;
-	for (int i = 0; i < m; i++)
+	if (file)
 	{
-		cout << "Sample #" << i << ": ";
-		sample.clear();
-		int x;
-		for (int j = 0; j < t; j++)
+		input >> m;
+		for (int i = 0; i < m; i++)
 		{
-			cin >> x;
-			sample.push_back(x);
+			cout << "Sample #" << i << ": ";
+			sample.clear();
+			int x;
+			for (int j = 0; j < t; j++)
+			{
+				input >> x;
+				sample.push_back(x);
+			}
+			hmm.pushSample(sample);
 		}
-		hmm.pushSample(sample);
+	}
+	else
+	{
+		cin >> m;
+		for (int i = 0; i < m; i++)
+		{
+			cout << "Sample #" << i << ": ";
+			sample.clear();
+			int x;
+			for (int j = 0; j < t; j++)
+			{
+				cin >> x;
+				sample.push_back(x);
+			}
+			hmm.pushSample(sample);
+		}
 	}
 	hmm.generateHMM(1.0);
 	system("pause");
+	input.close();
 }
